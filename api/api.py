600bf403832c6ec88926372a525bed6eed9b9d9d
TatoBig <tatobig@gmail.com>
2021-07-02 11:02:53 -0600

    Pruebas con la opcion 1

--- api/api.py
+++ api/api.py
@@ -1,10 +1,57 @@
 import time
 from flask import Flask
+from flask.wrappers import Request
 from flask_cors import CORS
+from flask import request
+import json
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/time')
 def get_current_time():
-    return {'time': time.time()}
+    matrix = [
+        [2, 5, -3, -1], 
+        [3, 0, 1, -3],
+        [-6, 0, -4, 9],
+        [4, 10, -4, -1]
+    ]
+
+    new_matrix = matrix
+
+    for i in range(0, 4):
+        for j in range(0, 3):
+            new_matrix[i].append(matrix[i][j])
+    result = 0
+    diagonal_up = 0
+    for i in range(0, 4):
+        if i == 3 or i == 1:
+            diagonal_up = diagonal_up * -1
+        result = diagonal_up + result
+        diagonal_up = 1
+        for j in range(0, 4):
+            diagonal_up = new_matrix[j][j+i] * diagonal_up
+    result = diagonal_up + result
+
+    result2 = 0
+    diagonal_down = 0
+    for i in range(0, 4):
+        if i == 3 or i == 1:
+            diagonal_down = diagonal_down * -1
+        result2 = diagonal_down + result2
+        diagonal_down = 1
+        for j in range(0, 4):
+            diagonal_down = new_matrix[3-j][j+i] * diagonal_down
+    result2 = diagonal_down + result2
+
+    result = result - result2
+
+    return {'result': new_matrix}
+
+@app.route("/test", methods=["POST"])
+def test():
+    result = 'test'
+    new_matrix = json.loads(request.json)
+    print(new_matrix("result"))
+    return {'result': 'test'}
+
--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,61 @@ import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react'
 
 export default function Home() {
-  const [currentTime, setCurrentTime] = useState(0)
+  const [currentTime, setCurrentTime] = useState('Cargando...')
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/time')
+  //     .then(res => res.body)
+  //     .then(body => {
+  //       const reader = body.getReader()
+  //       return new ReadableStream({
+  //         start(controller) {
+  //           function push() {
+  //             reader.read().then(({ done, value }) => {
+  //               if (done) {
+  //                 console.log('done', done)
+  //                 controller.close()
+  //                 return
+  //               }
+  //               controller.enqueue(value)
+  //               push()
+  //             })
+  //           }
+  //           push()
+  //         }
+  //       })
+  //     })
+  //     .then(stream => {
+  //       return new Response(stream, { headers: { "Content-Type": "text/html" } }).json()
+  //     })
+  //     .then(result => {
+  //       console.log(result)
+  //       setCurrentTime(`${result.time} ${result.test}`)
+  //     })
+  //     .catch(e => {
+  //       console.log(e)
+  //     })
+  // }, [])
 
   useEffect(() => {
-    fetch('http://localhost:5000/time')
-      .then(res => res.body)
-      .then(body => {
-        const reader = body.getReader()
-        return new ReadableStream({
-          start(controller) {
-            function push() {
-              reader.read().then(({ done, value }) => {
-                if (done) {
-                  console.log('done', done)
-                  controller.close()
-                  return
-                }
-                controller.enqueue(value)
-                push()
-              })
-            }
-            push()
-          }
-        })
-      })
-      .then(stream => {
-        return new Response(stream, { headers: { "Content-Type": "text/html" } }).json()
-      })
-      .then(result => {
-        setCurrentTime(result.time)
-      })
-      .catch(e => {
-        console.log(e)
-      })
+    fetch('http://localhost:5000/test')
+      .then(res => res.json()
+        .then(data => {
+          console.log(data)
+          // setCurrentTime(`${data.perro}`)
+        }))
+      .catch(e => console.log(e))
   }, [])
+
+  // useEffect(() => {
+  //   fetch('http://localhost:5000/time')
+  //     .then(res => res.json()
+  //       .then(data => {
+  //         console.log(data)
+  //       })
+  //     )
+  //     .catch(e => console.log(e))
+  // }, [])
 
   return (
     <div className={styles.container}>
